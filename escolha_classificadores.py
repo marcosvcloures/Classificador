53fe5c09d62b8ed3ad1eb6ac242480578620ec1a
Marcos Loures <marcosvcloures@gmail.com>
2017-07-16 02:08:25 -0300

    melhorando

--- escolha_classificadores.py
+++ escolha_classificadores.py
@@ -5,7 +5,24 @@ import warnings
 warnings.filterwarnings("ignore")
 
 if(__name__ == '__main__'):
-    x, y, height, width, pca = obterDados(dirname(abspath(__file__)) + "\\animais", MEDCINZA)
+    filtros = ["BASICO", "CINZA", "LIMIAR", "MEDIANA", "MEDCINZA", "MEDLIMIAR"]
 
-    for i in estimadores.split(' '):
-        print(np.mean(model_selection.cross_val_score(eval(i), x, y, n_jobs = 8)))
+    print("ANIMAIS")
+
+    for filtro in filtros:
+        print(filtro)
+
+        x, y, height, width, pca = obterDados(dirname(abspath(__file__)) + "\\animais", eval(filtro))
+
+        for estimador in estimadores.split(' '):
+            print(estimador, ": ", np.mean(model_selection.cross_val_score(eval(estimador), x, y, n_jobs = 8)))
+
+    print("SIMPSONS")
+
+    for filtro in filtros:
+        print(filtro)
+
+        x, y, height, width, pca = obterDados(dirname(abspath(__file__)) + "\\simpsons", eval(filtro))
+
+        for estimador in estimadores.split(' '):
+            print(estimador, ": ", np.mean(model_selection.cross_val_score(eval(estimador), x, y, n_jobs = 8)))
